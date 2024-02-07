493c8b8276ca720a9e5884c535193d94fc2e536c
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2024-02-07 16:59:40 +0300

    Move imeges to new folder, added a little magic and an example translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './assets/vite.svg'
 import './App.css'
+import { useTranslation } from 'react-i18next';
 
-function App() {
+function MyComponent() {
+  const { t, i18n } = useTranslation();
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+  const changeLanguage = (language: string) => () => {
+    i18n.changeLanguage(language);
+  };
+
+  return <>
+    <h1>{i18n.language}</h1>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+    <h1>Vite + React</h1>
+    <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
       </p>
-    </>
-  )
+    </div>
+    <p className="read-the-docs">
+      {t('learn')}
+    </p>
+    <button onClick={changeLanguage("en")}>English</button>
+    <button onClick={changeLanguage("ru")}>Русский</button>
+  </>
 }
 
-export default App
+// i18n translations might still be loaded by the http backend
+// use react's Suspense
+export default function App() {
+  return (
+    <Suspense fallback="loading">
+      <MyComponent />
+    </Suspense>
+  );
+}
---
+++ src/constants/paths.ts
@@ -0,0 +1,17 @@
+export const PATH = {
+  HOME: '/',
+  LOGIN: '/login',
+  REGISTER: '/signup',
+  CONTACT: '/contact',
+  ABOUT: '/about',
+  MAP: '/map/:id/zoom=:zoom/center=:centerX&:centerY&:centerZ/location=:locationId',
+  BUILD_ROUTE: '/buildroute/:idFrom&:idTo',
+  MY_PROFILE: '/my_profile',
+  USER: '/user/:id',
+  ERROR: '/error/:code',
+  FEEDBACK: '/feedback',
+  SETTINGS: '/settings',
+  TIMETABLE: '/timetable/group=:groupId/from=:fromDate&to=:toDate',
+  TIMETABLE_DAY: '/timetable/group=:groupId/day=:date',
+  EVENTS: '/events'
+};
